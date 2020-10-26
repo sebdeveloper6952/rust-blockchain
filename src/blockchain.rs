c6ed334144e451d07e84f5167079c63d03f72086
Sebas Arriola <sebdeveloper6952@gmail.com>
2020-10-26 15:00:19 -0600

    complete definitions according to tutorial.

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -2,3 +2,185 @@ extern crate time;
 extern crate serde;
 extern crate serde_json;
 extern crate sha2;
+
+use self::sha2::{Sha256, Digest};
+use std::fmt::Write;
+
+#[derive(Debug, Clone, Serialize)]
+struct Transaction {
+    sender: String,
+    receiver: String,
+    amount: f32
+}
+
+#[derive(Debug, Serialize)]
+pub struct Blockheader {
+    timestamp: i64,
+    nonce: u32,
+    difficulty: u32,
+    prev_hash: String,
+    merkle: String,
+}
+
+#[derive(Serialize, Debug)]
+pub struct Block {
+    header: Blockheader,
+    count: u32,
+    transactions:  Vec<Transaction>,
+}
+
+pub struct Blockchain {
+    blocks: Vec<Block>,
+    curr_trans: Vec<Transaction>,
+    difficulty: u32,
+    miner_addr: String,
+    reward: f32,
+}
+
+impl Blockchain {
+    pub fn new(miner_addr: String, difficulty: u32) -> Blockchain {
+        let mut blockchain = Blockchain {
+            blocks: Vec::new(),
+            curr_trans: Vec::new(),
+            difficulty,
+            miner_addr,
+            reward: 100.0,
+        };
+
+        // genesis block
+        blockchain.generate_block();
+
+        // return the new initialized blockchain
+        return blockchain;
+    }
+
+    pub fn new_transaction(&mut self, sender: String, receiver: String, amount: f32) -> bool {
+        self.curr_trans.push(Transaction{
+            sender,
+            receiver,
+            amount,
+        });
+
+        return true;
+    }
+
+    pub fn last_hash(&self) -> String {
+        let block = match self.blocks.last() {
+            Some(block) => block,
+            None => return String::from_utf8(vec![48, 64]).unwrap()
+        };
+
+        return Blockchain::hash(&block.header);
+    }
+
+    pub fn update_difficulty(&mut self, difficulty: u32) -> bool {
+        self.difficulty = difficulty;
+
+        return true;
+    }
+
+    pub fn update_reward(&mut self, reward: f32) -> bool {
+        self.reward = reward;
+        
+        return true;
+    }
+
+    pub fn generate_block(&mut self) -> bool {
+        let header = Blockheader {
+            timestamp: time::now().to_timespec().sec,
+            nonce: 0,
+            difficulty: self.difficulty,
+            prev_hash: self.last_hash(),
+            merkle: String::new(),
+        };
+
+        let reward_trans = Transaction {
+            sender: String::from("Root"),
+            receiver: self.miner_addr.clone(),
+            amount: self.reward,
+        };
+
+        let mut block = Block {
+            header: header,
+            count: 0,
+            transactions: vec![],
+        };
+
+        block.transactions.push(reward_trans);
+        block.transactions.append(&mut self.curr_trans);
+        block.count = block.transactions.len() as u32;
+        block.header.merkle = Blockchain::get_merkle(block.transactions.clone());
+        Blockchain::proof_of_work(&mut block.header);
+
+        println!("{:#?}", &block);
+        self.blocks.push(block);
+        
+        return true;
+    }
+
+    fn get_merkle(curr_trans: Vec<Transaction>) -> String {
+        let mut merkle = Vec::new();
+
+        for t in &curr_trans {
+            let hash = Blockchain::hash(t);
+            merkle.push(hash);
+        }
+
+        if merkle.len() % 2 == 1 {
+            let last = merkle.last().cloned().unwrap();
+            merkle.push(last);
+        }
+
+        while merkle.len() > 1 {
+            let mut h1 = merkle.remove(0);
+            let mut h2 = merkle.remove(0);
+            h1.push_str(&mut h2);
+            let nh = Blockchain::hash(&h1);
+            merkle.push(nh);
+        }
+
+        // the last hash is the merkle root
+        return merkle.pop().unwrap();
+    }
+
+    pub fn proof_of_work(header: &mut Blockheader) {
+        loop {
+            let hash = Blockchain::hash(header);
+            let slice = &hash[..header.difficulty as usize];
+            match slice.parse::<u32>() {
+                Ok(val) => {
+                    if val != 0 {
+                        header.nonce += 1;
+                    } else {
+                        println!("Block hash: {}", hash);
+                        break;
+                    }
+                },
+                Err(_) => {
+                    header.nonce += 1;
+                    continue;
+                }
+            }
+        }
+    }
+
+    pub fn hash<T: serde::Serialize>(item: &T) -> String {
+        let input = serde_json::to_string(&item).unwrap();
+        let mut hasher = Sha256::default();
+        hasher.update(input.as_bytes());
+        let result = hasher.finalize();
+        let vec_res = result.to_vec();
+
+        return Blockchain::hex_to_string(vec_res.as_slice());
+    }
+
+    pub fn hex_to_string(vec_res: &[u8]) -> String {
+        let mut s = String::new();
+        for byte in vec_res {
+            write!(&mut s, "{:x}", byte).expect("unable to write");
+        }
+
+        return s;
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
+#[macro_use]
+extern crate serde_derive;
+
+use std::io;
+use std::process;
+use std::io::Write;
+
+mod blockchain;
+
 fn main() {
-    println!("Hello, world!");
+    let mut miner_addr = String::new();
+    let mut difficulty = String::new();
+    let mut choice = String::new();
+
+    print!("input miner address: ");
+    io::stdout().flush();
+    io::stdin().read_line(&mut miner_addr);
+
+    print!("difficulty: ");
+    io::stdout().flush();
+    io::stdin().read_line(&mut difficulty);
+    let diff = difficulty.trim().parse::<u32>().expect("difficulty must be an integer.");
+
+    println!("generating genesis block...");
+    let mut blockchain = blockchain::Blockchain::new(miner_addr.trim().to_string(), diff);
+
+    loop {
+        println!("Menu");
+        println!("1) New Transaction");
+        println!("2) Mine block");
+        println!("3) Change Difficulty");
+        println!("4) Change Reward");
+        println!("0) Exit");
+        print!("Enter your choice: ");
+        io::stdout().flush();
+        choice.clear();
+        io::stdin().read_line(&mut choice);
+        println!("");
+
+        match choice.trim().parse().unwrap() {
+            0 => {
+                print!("exiting program...");
+                process::exit(0);
+            },
+            1 => {
+                let mut sender = String::new();
+                let mut receiver = String::new();
+                let mut amount = String::new();
+                let trans_res;
+
+                print!("sender address: ");
+                io::stdout().flush();
+                io::stdin().read_line(&mut sender);
+                print!("receiver address: ");
+                io::stdout().flush();
+                io::stdin().read_line(&mut receiver);
+                print!("amount: ");
+                io::stdout().flush();
+                io::stdin().read_line(&mut amount);
+                
+                trans_res = blockchain.new_transaction(
+                    sender.trim().to_string(),
+                    receiver.trim().to_string(),
+                    amount.trim().parse().unwrap()
+                );
+            },
+            _ => println!("invalid option..."),
+        }
+    }
 }
